4f1729084532aa24748af75daeb3b4f3865b8fc9
Matthew Akuhata <matthewakuhata@gmail.com>
2022-08-06 17:17:46 +1000

    hide api key

--- src/utils/isValidWord.js
+++ src/utils/isValidWord.js
@@ -4,7 +4,7 @@ import axois from 'axios';
 const isValidWord = (word) => {
    axois.get(`https://api.api-ninjas.com/v1/dictionary?word=${word}`, {
       headers:
-         { 'X-Api-Key': '1IzsQ9vc7YFZwCT1o+Q8lA==CJAvzKDE4XQqBWgY' },
+         { 'X-Api-Key': process.env.API_KEY },
    }).then(result => result.valid);
 };
 
