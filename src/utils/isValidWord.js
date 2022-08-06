6bbb5f6e8819130e1ce5d0b1f93403de06fe2c8f
Matthew Akuhata <matthewakuhata@gmail.com>
2022-08-06 17:51:06 +1000

    REMOVE CONSOLE LOG

--- src/utils/isValidWord.js
+++ src/utils/isValidWord.js
@@ -2,10 +2,9 @@ import { useState, createContext, useEffect } from 'react';
 import axois from 'axios';
 
 const isValidWord = (word) => {
-   console.log(process.env);
    axois.get(`https://api.api-ninjas.com/v1/dictionary?word=${word}`, {
       headers:
-         { 'X-Api-Key': process.env.RAZZLE_API_KEY },
+         { 'X-Api-Key': '1IzsQ9vc7YFZwCT1o+Q8lA==CJAvzKDE4XQqBWgY' },
    }).then(result => result.valid);
 };
 
