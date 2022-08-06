2f8ccae58a7aad54b3ba156362e820c80ce93abe
Matthew Akuhata <matthewakuhata@gmail.com>
2022-08-06 17:43:59 +1000

    Remove hard return

--- src/utils/isValidWord.js
+++ src/utils/isValidWord.js
@@ -2,7 +2,7 @@ import { useState, createContext, useEffect } from 'react';
 import axois from 'axios';
 
 const isValidWord = (word) => {
-   console.log(process.env); return false;
+   console.log(process.env);
    axois.get(`https://api.api-ninjas.com/v1/dictionary?word=${word}`, {
       headers:
          { 'X-Api-Key': process.env.RAZZLE_API_KEY },
